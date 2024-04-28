b0140691d14dedf36851cbccfa27542f16ea6b5d
José Luis Tanicuchí Cadena <josluistanic99@outlook.es>
2024-04-27 20:53:16 -0500

    Create table component

---
+++ app/Livewire/EnergyDrinksTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class EnergyDrinksTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.energy-drinks-table');
+    }
+}
---
+++ resources/views/livewire/energy-drinks-table.blade.php
@@ -0,0 +1,43 @@
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-neutral-50 dark:bg-neutral-700 dark:text-neutral-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Brand
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Description
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Sugar free
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Price
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    <span class="sr-only">Actions</span>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr
+                class="border-b odd:bg-white odd:dark:bg-neutral-900 even:bg-neutral-50 even:dark:bg-neutral-800 dark:border-gray-700 ">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Red Bull
+                </th>
+                <td class="px-6 py-4">
+                    Red Bull Energy Drink
+                </td>
+                <td class="px-6 py-4">
+                    check_icon
+                </td>
+                <td class="px-6 py-4">
+                    2.99
+                </td>
+                <td class="px-6 py-4 text-right">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
