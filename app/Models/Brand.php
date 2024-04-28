c1c197b82a1ad7e64757c8f2edb9e6db84e40899
José Luis Tanicuchí Cadena <josluistanic99@outlook.es>
2024-04-27 20:54:57 -0500

    Add model relationship

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Brand extends Model
 {
     use HasFactory;
+
+    /**
+     * Get all of the energyDrinks for the Brand
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function energyDrinks(): HasMany
+    {
+        return $this->hasMany(EnergyDrink::class);
+    }
 }
--- app/Models/EnergyDrink.php
+++ app/Models/EnergyDrink.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EnergyDrink extends Model
 {
     use HasFactory;
+    /**
+     * Get the brand that owns the EnergyDrink
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
 }
